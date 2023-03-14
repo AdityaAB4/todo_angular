27c4dfd56bbdc11a9d0cd42e26571a7a8806c977
AdityaAB4 <adityabarde4ab@gmail.com>
2023-03-14 23:34:53 +0530

    first commit

---
+++ src/app/Mycomponents/todo-item/todo-item.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { TodoItemComponent } from './todo-item.component';
+
+@NgModule({
+  declarations: [TodoItemComponent],
+  imports: [CommonModule],
+})
+export class TodoItemModule {}
